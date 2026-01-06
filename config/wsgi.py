2582e889805574e40b496aed1a09a313c1b2122b
UZIMA MATEENE ZENON <uzimamzenon@gmail.com>
2026-01-07 00:11:20 +0100

    Reparation : page accueil, static et fetch relatif

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from messagerie.views import page_accueil # Importe ta vue d'accueil
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
