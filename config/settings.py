ae9cd665cfe091994b6e788e6666516c3782dbe1
UZIMA MATEENE ZENON <uzimamzenon@gmail.com>
2026-01-07 00:45:37 +0100

    Correction finale Allowed Hosts pour orphelin-asbl

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://orphelin-asbl.onrender.com',
     'https://orphelin-prioritee-backend.onrender.com',
     'http://127.0.0.1:8000'
+    'http://localhost:8000'
 ]
 
 # --- SÉCURITÉ HTTPS SUR RENDER ---
